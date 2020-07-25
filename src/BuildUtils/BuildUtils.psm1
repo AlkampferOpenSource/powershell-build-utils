ed83219c816af78866ea439dcc021e7371cee20e
Gian Maria Ricci <alkampfer@nablasoft.com>
2020-07-25 11:38:16 +0200

    added location of nuget.exe

--- src/BuildUtils/BuildUtils.psm1
+++ src/BuildUtils/BuildUtils.psm1
@@ -290,7 +290,33 @@ function Get-LatestMsbuildLocation
     return $msbuildLocation
 }
 
-Export-ModuleMember -Function * -Cmdlet *
+<#
+.SYNOPSIS
+Get location of latest nuget in temp folder, if nuget.exe is not found it will download
+
+.DESCRIPTION
+
+.EXAMPLE
+
+$nugetLocation = Get-NugetLocation
+set-alias nuget $nugetLocation 
+
+nuget yoursolution.sln
+#>
+function Get-NugetLocation
+{
+  Param 
+  (
+  )
+    
+    $nugetLocation = "$env:TEMP\nuget.exe"
+    if (!(Test-Path -Path $nugetLocation)) {
+
+      Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile $nugetLocation
+    }
+
+    return $nugetLocation
+}
 
 Export-ModuleMember -Function * -Cmdlet *
   
