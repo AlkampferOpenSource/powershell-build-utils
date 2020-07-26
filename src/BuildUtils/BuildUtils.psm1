bc123a9f933798e8ca681e9a9ed4c249b3421f8d
Gian Maria Ricci <alkampfer@nablasoft.com>
2020-07-26 18:43:08 +0200

    Fixed Get-LatestMsbuildLocation

--- src/BuildUtils/BuildUtils.psm1
+++ src/BuildUtils/BuildUtils.psm1
@@ -284,9 +284,23 @@ function Get-LatestMsbuildLocation
 {
   Param 
   (
+    [bool] $allowPreviewVersions = $false
   )
-    $latestVsInstallationInfo = Get-VSSetupInstance -All | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
-    $msbuildLocation = "$($latestVsInstallationInfo.InstallationPath)\MSBuild\Current\Bin\msbuild.exe"
+    if ($allowPreviewVersions) {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All -Prerelease | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    } else {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    }
+    Write-Host "Latest version installed is $($latestVsInstallationInfo.InstallationVersion)"
+    if ($latestVsInstallationInfo.InstallationVersion -like "15.*") {
+      $msbuildLocation = "$($latestVsInstallationInfo.InstallationPath)\MSBuild\15.0\Bin\msbuild.exe"
+    
+      Write-Host "Located msbuild for Visual Studio 2017 in $msbuildLocation"
+    } else {
+      $msbuildLocation = "$($latestVsInstallationInfo.InstallationPath)\MSBuild\Current\Bin\msbuild.exe"
+      Write-Host "Located msbuild in $msbuildLocation"
+    }
+
     return $msbuildLocation
 }
 
