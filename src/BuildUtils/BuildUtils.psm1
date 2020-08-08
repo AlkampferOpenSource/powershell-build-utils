0d3e546ced7bd409a86b222f1eb3d5bea7f74dd3
Gian Maria Ricci <alkampfer@nablasoft.com>
2020-08-08 09:09:24 +0200

    fixed get nunit runner

--- src/BuildUtils/BuildUtils.psm1
+++ src/BuildUtils/BuildUtils.psm1
@@ -332,5 +332,38 @@ function Get-NugetLocation
     return $nugetLocation
 }
 
+<#
+.SYNOPSIS
+Get / download nunit test runners and return the folder
+with the console runner
+
+.DESCRIPTION
+
+.EXAMPLE
+
+$nunitConsoleRunner = GEt-NunitTestsConsoleRunner
+set-alias nunit "$nunitConsoleRunner\nunit3-console-exe"
+
+#>
+function Get-NunitTestsConsoleRunner
+{
+  Param 
+  (
+  )
+    
+    $nunitLocation = "$env:TEMP\nunitrunners"
+    if (!(Test-Path -Path $nunitLocation)) {
+
+      $nugetLocation = Get-NugetLocation
+      set-alias nunitinternal $nugetLocation
+      nunitinternal install NUnit.Runners -OutputDirectory $nunitLocation
+    }
+
+    #Now we need to locate the console runner
+    $consoleRunner = Get-ChildItem -Path $nunitLocation -Name nunit3-console.exe -Recurse 
+    Write-Host "Found nunit runner in $nunitLocation\$consoleRunner"
+    return "$nunitLocation\$consoleRunner"
+}
+
 Export-ModuleMember -Function * -Cmdlet *
   
