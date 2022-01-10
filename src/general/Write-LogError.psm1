a4d41cbb90526bb9e1a7d8da8691ff477ba9e4af
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2022-01-10 18:00:44 +0100

    Added some log useful functions

---
+++ src/general/Write-LogError.psm1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+Write a log error CI enabled
+
+.DESCRIPTION
+Simply write a log error to the console and write also a special
+command for continous integration engine to have the script signal
+an error in the step. Remember to set $ci_engine to the correct value
+for your CI engine
+
+.EXAMPLE
+
+Write-LogError -message "Failed to foo the bar." -$haltExecution $true
+
+.NOTES
+$ci_engine valid values are ["azdo", "github"]
+
+#>
+function Write-LogError(
+    [string] $message,
+    [bool] $haltExecution = $false) 
+{
+    Write-Error $message
+    # Now trying to detect if we are running inside a CI engine, to try to emit correct error message
+
+    if ("azdo" -eq $ci_engine)
+    {
+        Write-Host "##vso[task.logissue type=error]$message"
+    }
+    elseif ("github" -eq $ci_engine)
+    {
+        Write-Host "::error::$message"
+    }
+
+    if ($haltExecution)
+    {
+       exit (1)
+    }
+}
